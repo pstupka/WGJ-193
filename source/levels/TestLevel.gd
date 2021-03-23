775eccc4b2bae72a12924e3fcc81fbf140d1ebd5
Patryk Stupka <patryk.stupka@gmail.com>
2021-03-23 10:55:13 +0100

    Added Acceptable terrain destruction

--- source/levels/TestLevel.gd
+++ source/levels/TestLevel.gd
@@ -7,9 +7,14 @@ var explosion : PackedScene = preload("res://source/environment/Explosion.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	var init_poly = Polygon2D.new()
 	init_poly.set_polygon($initPolygon.polygon)
+	$initPolygon.queue_free()
 	var clipable_body = ClipableStaticBody.new(init_poly)
+	var texture = ImageTexture.new()
+	texture.load("res://icon.png")
+	clipable_body.set_texture(texture)
 	add_child(clipable_body)
 
 
@@ -22,11 +27,16 @@ func _input(event):
 			add_child(explosion_instance)
 			for node in explosion_instance.explosion_polygon.polygon:
 				node_array.append(node + get_global_mouse_position())
+				
+			print(get_tree().get_nodes_in_group("Clipable").size())
 			for clippable in get_tree().get_nodes_in_group("Clipable"):
 				var leftovers : Array = clippable.clip_static_body(node_array)
 				for area in leftovers:
 					var polygon = Polygon2D.new()
 					polygon.set_polygon(area)
 					var body = ClipableStaticBody.new(polygon)
+					var texture = ImageTexture.new()
+					texture.load("res://icon.png")
+					body.set_texture(texture)
 					add_child(body)
 
